d126827f7e065f2ed3ad78341e852a821c37bda2
root <root@limitoo.build>
2021-11-10 06:51:41 +0000

    update auto shell.

--- auto.sh
+++ auto.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-PIDS=$(ps -ef | grep python3.9 | grep -v grep | awk '{print $2}')
-
+PIDS=$(ps -ef | grep bbc.py | grep -v grep | awk '{print $2}')
 if [ "$PIDS" = "" ]; then
 	/usr/bin/python3.9 /root/limitoo/news_py/bbc.py
-	/usr/bin/python3.9 /root/limitoo/news_py/au_abc.py
-	/usr/bin/python3.9 /root/limitoo/news_py/us_newsweek.py
-	/usr/bin/python3.9 /root/limitoo/news_py/us_nytimes.py
-	/usr/bin/python3.9 /root/limitoo/news_py/cbsnews.py
-	/usr/bin/python3.9 /root/limitoo/news_py/rnz.py
-	/usr/bin/python3.9 /root/limitoo/news_py/aljazeera.py
-else
-        echo "Spider Build is runing."
-        echo $PIDS
 fi
 
+PIDS=$(ps -ef | grep au_abc.py | grep -v grep | awk '{print $2}')
+if [ "$PIDS" = "" ]; then
+	/usr/bin/python3.9 /root/limitoo/news_py/au_abc.py
+fi
 
+PIDS=$(ps -ef | grep us_newsweek.py | grep -v grep | awk '{print $2}')
+if [ "$PIDS" = "" ]; then
+	/usr/bin/python3.9 /root/limitoo/news_py/us_newsweek.py
+fi
+
+PIDS=$(ps -ef | grep us_nytimes.py | grep -v grep | awk '{print $2}')
+if [ "$PIDS" = "" ]; then
+	/usr/bin/python3.9 /root/limitoo/news_py/us_nytimes.py
+fi
+
+PIDS=$(ps -ef | grep cbsnews.py | grep -v grep | awk '{print $2}')
+if [ "$PIDS" = "" ]; then
+	/usr/bin/python3.9 /root/limitoo/news_py/cbsnews.py
+fi
+PIDS=$(ps -ef | grep rnz.py | grep -v grep | awk '{print $2}')
+if [ "$PIDS" = "" ]; then
+	/usr/bin/python3.9 /root/limitoo/news_py/rnz.py
+fi
+
+PIDS=$(ps -ef | grep aljazeera.py  | grep -v grep | awk '{print $2}')
+if [ "$PIDS" = "" ]; then
+	/usr/bin/python3.9 /root/limitoo/news_py/aljazeera.py
+fi
